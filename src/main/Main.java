e45cf3a521ac88319079371eeebc9c407dd1e52a
Sofia-Power <power.sofiaet36@gmail.com>
2025-08-11 17:26:07 -0300

    Código de main actualizado

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package src.main;
+
 import src.vistas.*;
+import src.controlador.*;
 
 public class Main {
     public static void main(String[] args) {
+        Controlador controller = new Controlador();
         ventana_principal vent_princ = new ventana_principal();
         vent_princ.setVisible(true);
     }
