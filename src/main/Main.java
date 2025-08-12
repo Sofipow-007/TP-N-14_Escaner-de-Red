5449b22a8969221c0ba722b5c46c2292aed6903f
Sofia-Power <power.sofiaet36@gmail.com>
2025-08-12 12:27:20 -0300

    Código del main actualizado

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,17 @@ package src.main;
 
 import src.vistas.*;
 import src.controlador.*;
+import src.modelo.ComienzoScanner;
 
 public class Main {
     public static void main(String[] args) {
         Controlador controller = new Controlador();
         ventana_principal vent_princ = new ventana_principal();
         vent_princ.setVisible(true);
+
+        ComienzoScanner scanner = new ComienzoScanner("127.0.0.1", "www.google.com");
+        boolean resultado = scanner.hacerPing("127.0.0.1"); //Prueba de Ping
+        System.out.println("Ping a 127.0.0.1 " + resultado);
+        scanner.obtenerNombreIP("www.google.com"); //Prueba de nslookup
     }
 }
