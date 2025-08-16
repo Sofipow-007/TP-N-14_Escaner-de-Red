60dda5f7c221327236b530d6de79ed7163f8a440
Sofia-Power <power.sofiaet36@gmail.com>
2025-08-16 20:31:31 -0300

    Clase listaEquiposRed terminada y documentada

--- src/modelo/listaEquiposRed.java
+++ src/modelo/listaEquiposRed.java
@@ -1,23 +1,24 @@
 package src.modelo;
 
-import java.util.ArrayList; // Lista que va a servis para guardar los equipos de red si el usuario lo solicita
+import java.util.ArrayList; // Se importa la librería java.util mencionando a los métodos ArrayList
+// Esto nos va a servir para que se guarden en una lista interna cada equipo que se calcule en el escáner
 
 public class listaEquiposRed { // Esta clase va a servir para poder mantener un control de todos los equipos que se guarden en una lista
     private ArrayList<ResultadoScanner> listaEquipos;
 
     public listaEquiposRed(){
-        listaEquipos = new ArrayList<>();
+        listaEquipos = new ArrayList<>(); // Se declara la ArrayList listaEquipos
     }
     
-    public void agregarEquipo(ResultadoScanner equipoRed){
+    public void agregarEquipo(ResultadoScanner equipoRed){ // Función que va a servir para  agregar equipos a la lista
         listaEquipos.add(equipoRed);
     }
 
-    public ArrayList<ResultadoScanner> getListaEquipos(){
+    public ArrayList<ResultadoScanner> getListaEquipos(){ // Devuelve los componentes de la lista
         return listaEquipos;
     }
 
-    public void limpiarLista(){
+    public void limpiarLista(){ // Limpia todos los resultados de la lista para agregar nuevos equipos de red dentro de ella
         listaEquipos.clear();
     }
 }
