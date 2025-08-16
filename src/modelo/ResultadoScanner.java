c74f70378035dbba61f70b2dd5e38759d84fbac0
Sofia-Power <power.sofiaet36@gmail.com>
2025-08-16 20:31:02 -0300

    Clase ResultadoScanner terminada y documentada

--- src/modelo/ResultadoScanner.java
+++ src/modelo/ResultadoScanner.java
@@ -1,12 +1,12 @@
 package src.modelo;
 
-public class ResultadoScanner { // En esta clase se van a guardar los resultados de nuestro escáner de red, devolviendonos la ip encontrada (la que se ingresa como ip final) con su nombre de equipo y si está conectado.
+public class ResultadoScanner { // En esta clase se van a guardar los resultados de nuestro escáner de red, devolviendonos el equipo con la ip encontrada, su nombre de equipo y si está conectado.
     // Además, se va a mostrar el tiempo de respuesta para que esta solicitud se cumpla
 
     private String ipResult; // Direccion IP
     private String nombreEquipo; // Nombre de host del equipo
-    private boolean conectado; // Si está conectado o no
-    private int tiempoRespuesta; // Tiempo de respuesta (1 ms)
+    private boolean conectado; // Si está conectado o no (true o false)
+    private int tiempoRespuesta; // Tiempo de respuesta (por ejemplo, 1ms)
 
     // Elaboración del constructor de ResultadoScanner
 
@@ -19,31 +19,31 @@ public class ResultadoScanner { // En esta clase se van a guardar los resultados
 
     // Elaboración de los getters y setters de la clase ResultadoScanner
 
-    public String getIpResult(){
+    public String getIpResult(){ // Devuelve la IP del equipo
         return ipResult;
     }
 
-    public void setIpResult(String ipResult){
+    public void setIpResult(String ipResult){ // Modifica la IP del equipo
         this.ipResult = ipResult;
     }
 
-    public String getNombreEquipo(){
+    public String getNombreEquipo(){ // Devuelve el nombre del equipo
         return nombreEquipo;
     }
 
-    public void setNombreEquipo(String nombreEquipo){
+    public void setNombreEquipo(String nombreEquipo){ // Modifica el nombre del equipo
         this.nombreEquipo = nombreEquipo;
     }
 
-    public boolean isConectado(){
+    public boolean isConectado(){ // Verifica el estado del booleano
         return conectado;
     }
 
-    public int getTiempoRespuesta(){
+    public int getTiempoRespuesta(){ // Devuelve el tiempo de respuesta
         return tiempoRespuesta;
     }
 
-    public void setTiempoRespuesta(int tiempoRespuesta){
+    public void setTiempoRespuesta(int tiempoRespuesta){ // Modifica el tiempo de respuesta
         this.tiempoRespuesta = tiempoRespuesta;
     }
 
