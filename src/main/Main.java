9458a2a00998879f1f7341feeab2334d319545ec
Sofia-Power <power.sofiaet36@gmail.com>
2025-08-16 20:19:17 -0300

    La clase Main está terminada y documentada correctamente

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,10 +2,10 @@ package src.main;
 
 import src.controlador.*;
 
-public class Main {
+public class Main { // Esta clase se va a utilizar para ejecutar el controlador, por lo que se va a lanzar la interfaz gráfica cuando la ejecutemos
     public static void main(String[] args) {
         
-        new Controlador();
+        new Controlador(); // Se llama a la clase Controlador para empezar
         
     }
 }
