7f6229ff4c08c385ee9c050095e8bffe11a758bf
Sofia-Power <power.sofiaet36@gmail.com>
2025-09-27 17:23:39 -0300

    Actualización: Nueva clase 'panelNetStat' para visualizar algunas estadísticas

--- src/vistas/panelNetStat.java
+++ src/vistas/panelNetStat.java
@@ -6,19 +6,21 @@ import javax.swing.*;
 
 import src.controlador.*;
 
-public class panelNetStat extends JFrame implements ActionListener{
-    private Controlador controller;
+public class panelNetStat extends JFrame implements ActionListener { // Ventana que va a mostrar nueva información en la aplicación
+    private ControladorNetStat controller;
 
     private JTextArea infoComando;
     private JButton conexionesActivas = new JButton("Ver Conexiones Activas");
+    private JButton infoRouter = new JButton("Mostrar estadísticas de routing");
+    private JButton protocolosStats = new JButton("Mostrar protocolos de red");
 
-    public panelNetStat(Controlador controller) {
+    public panelNetStat() {
         setTitle("Panel de herramientas NetStat");
-        setSize(800, 500);
+        setSize(850, 500);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
-        this.controller = controller;
+        controller = new ControladorNetStat(this);
 
         JPanel laminaNet = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -26,7 +28,8 @@ public class panelNetStat extends JFrame implements ActionListener{
         gbc.fill = GridBagConstraints.CENTER;
         gbc.insets = new Insets(10, 10, 10, 10);
 
-        JLabel introduccion = new JLabel("Bienvenido a la ventana de comandos NetStat. A continuación, elija alguno de los botones para mostrar estadísticas de red");
+        JLabel introduccion = new JLabel(
+                "Bienvenido a la ventana de comandos NetStat. A continuación, elija alguno de los botones para mostrar estadísticas de red");
         gbc.gridy = 0;
         gbc.gridx = 0;
         gbc.gridwidth = 4;
@@ -40,9 +43,14 @@ public class panelNetStat extends JFrame implements ActionListener{
         laminaNet.add(scroll, gbc);
 
         conexionesActivas.addActionListener(this);
+        infoRouter.addActionListener(this);
+        protocolosStats.addActionListener(this);
 
         JPanel panelBotones = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 0));
         panelBotones.add(conexionesActivas);
+        panelBotones.add(infoRouter);
+        panelBotones.add(protocolosStats);
+
         gbc.gridy = 2;
         laminaNet.add(panelBotones, gbc);
 
@@ -50,17 +58,29 @@ public class panelNetStat extends JFrame implements ActionListener{
 
     }
 
+    public JButton getBtnConexiones() {
+        return conexionesActivas;
+    }
+
+    public void setResultado(String texto) {
+        infoComando.setText(texto);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            Object source = e.getSource();
-            if (source == conexionesActivas) {
-                String resultado = controller.commandNetStat();
-                infoComando.setText(resultado);
-            }
+        Object source = e.getSource();
+
+        if (source == conexionesActivas) {
+            controller.mostrarConexiones();
         }
-        catch (Exception ex) {
-            JOptionPane.showMessageDialog(this, "Tuvo un error en su programa.\n" + ex, "Error encontrado", JOptionPane.ERROR_MESSAGE);
+
+        if (source == infoRouter) {
+            controller.mostrarRouting();
         }
+
+        if (source == protocolosStats) {
+            controller.mostrarProtocolos();
+        }
+
     }
 }
