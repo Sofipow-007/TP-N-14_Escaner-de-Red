3fbec7948f8d9fcedb447302a09dc740c2bf0413
Sofia-Power <power.sofiaet36@gmail.com>
2025-09-28 17:29:21 -0300

    Clase 'panelNetStat' actualizada

--- src/vistas/panelNetStat.java
+++ src/vistas/panelNetStat.java
@@ -6,17 +6,20 @@ import javax.swing.*;
 
 import src.controlador.*;
 
-public class panelNetStat extends JFrame implements ActionListener { // Ventana que va a mostrar nueva información en la aplicación
+public class panelNetStat extends JFrame implements ActionListener { // Ventana que va a mostrar nueva información en la
+                                                                     // aplicación
     private ControladorNetStat controller;
 
     private JTextArea infoComando;
+
     private JButton conexionesActivas = new JButton("Ver Conexiones Activas");
     private JButton infoRouter = new JButton("Mostrar estadísticas de routing");
     private JButton protocolosStats = new JButton("Mostrar protocolos de red");
+    private JButton limpiarVentana = new JButton("Limpiar área de texto");
 
     public panelNetStat() {
         setTitle("Panel de herramientas NetStat");
-        setSize(850, 500);
+        setSize(1020, 550);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
@@ -30,13 +33,14 @@ public class panelNetStat extends JFrame implements ActionListener { // Ventana 
 
         JLabel introduccion = new JLabel(
                 "Bienvenido a la ventana de comandos NetStat. A continuación, elija alguno de los botones para mostrar estadísticas de red");
+
         gbc.gridy = 0;
         gbc.gridx = 0;
         gbc.gridwidth = 4;
         laminaNet.add(introduccion, gbc);
 
         infoComando = new JTextArea(20, 50);
-        infoComando.setEditable(false);
+        infoComando.setEditable(false); // No se puede modificar
         JScrollPane scroll = new JScrollPane(infoComando);
 
         gbc.gridy = 1;
@@ -45,11 +49,13 @@ public class panelNetStat extends JFrame implements ActionListener { // Ventana 
         conexionesActivas.addActionListener(this);
         infoRouter.addActionListener(this);
         protocolosStats.addActionListener(this);
+        limpiarVentana.addActionListener(this);
 
         JPanel panelBotones = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 0));
         panelBotones.add(conexionesActivas);
         panelBotones.add(infoRouter);
         panelBotones.add(protocolosStats);
+        panelBotones.add(limpiarVentana);
 
         gbc.gridy = 2;
         laminaNet.add(panelBotones, gbc);
@@ -80,6 +86,10 @@ public class panelNetStat extends JFrame implements ActionListener { // Ventana 
 
         if (source == protocolosStats) {
             controller.mostrarProtocolos();
+        }
+
+        if (source == limpiarVentana) {
+            infoComando.setText("");
         }
 
     }
